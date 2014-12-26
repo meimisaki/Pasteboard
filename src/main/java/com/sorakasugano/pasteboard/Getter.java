f10e528902a25ed988b88fc2d8d80ebbfa3c507e
meimisaki <meimisaki@gmail.com>
2014-12-26 18:59:13 +0800

    returns null if key does not exist

--- src/main/java/com/sorakasugano/pasteboard/Getter.java
+++ src/main/java/com/sorakasugano/pasteboard/Getter.java
@@ -7,7 +7,9 @@ import com.sorakasugano.pasteboard.Reader;
 public class Getter extends Reader<Map<String, String>> {
     @Override
     public Map<String, String> call(Jedis jedis) throws Exception {
-        Map<String, String> out = jedis.hgetAll(type + ":" + id);
+        String key = type + ":" + id;
+        if (!jedis.exists(key)) return null;
+        Map<String, String> out = jedis.hgetAll(key);
         out.put("id", id);
         return out;
     }
