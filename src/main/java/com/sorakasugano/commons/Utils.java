91c9c4876044bf663840a4e87116fdb7acf8615a
meimisaki <meimisaki@gmail.com>
2014-12-28 03:22:59 +0800

    change default behavior of Utils.JSONEncode

--- src/main/java/com/sorakasugano/commons/Utils.java
+++ src/main/java/com/sorakasugano/commons/Utils.java
@@ -5,6 +5,6 @@ import com.google.gson.*;
 public class Utils {
     private static final Gson gson = new Gson();
     public static String JSONEncode(Object in) {
-        return in == null ? "" : gson.toJson(in);
+        return gson.toJson(in == null ? "" : in);
     }
 }
