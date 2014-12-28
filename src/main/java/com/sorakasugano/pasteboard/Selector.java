eff14bd4ba44f5467844f710d256497a7fb26fb2
meimisaki <meimisaki@gmail.com>
2014-12-28 16:37:53 +0800

    implement database selector

--- src/main/java/com/sorakasugano/pasteboard/Selector.java
+++ src/main/java/com/sorakasugano/pasteboard/Selector.java
@@ -2,14 +2,27 @@ package com.sorakasugano.pasteboard;
 
 import java.util.*;
 import redis.clients.jedis.*;
+import com.sorakasugano.pasteboard.Adapter;
 import com.sorakasugano.pasteboard.Reader;
+import com.sorakasugano.pasteboard.Getter;
 
-public class Selector extends Reader<Map<String, String>[]> {
-    private int offset = 0;
-    private int limit = -1;
-    private String keyword = null;
+public class Selector extends Reader<List<Map<String, String>>> {
+    public String subtype = null;
+    public int offset = 0;
+    public int limit = -1;
+    private String list() {
+        return (type == null ? "" : type + ":" + id + ":") + subtype + "-list";
+    }
     @Override
-    public Map<String, String>[] call(Jedis jedis) {
-        return null;
+    public List<Map<String, String>> call(Jedis jedis) throws Exception {
+        Set<String> set = jedis.zrevrange(list(), offset, limit < 0 ? -1 : offset + limit);
+        List<Map<String, String>> out = new LinkedList<Map<String, String>>();
+        for (String id : set) {
+            Getter getter = new Getter();
+            getter.type = subtype;
+            getter.id = id;
+            out.add(Adapter.invoke(getter));
+        }
+        return out;
     }
 }
