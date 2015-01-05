c44707cd5d238eeb827d7d31b96baa67cb93aee1
meimisaki <meimisaki@gmail.com>
2015-01-06 02:56:58 +0800

    fix encoding bug with some tricks

--- src/main/java/com/sorakasugano/pasteboard/Getter.java
+++ src/main/java/com/sorakasugano/pasteboard/Getter.java
@@ -10,6 +10,12 @@ public class Getter extends Reader<Map<String, String>> {
         String key = type + ":" + id;
         if (!jedis.exists(key)) return null;
         Map<String, String> out = jedis.hgetAll(key);
+        for (Map.Entry<String, String> entry : out.entrySet()) {
+            char[] chars = entry.getValue().toCharArray();
+            byte[] bytes = new byte[chars.length];
+            for (int i = 0 ; i < chars.length ; ++i) bytes[i] = (byte)chars[i];
+            out.put(entry.getKey(), new String(bytes, "UTF-8"));
+        }
         out.put("id", id);
         return out;
     }
